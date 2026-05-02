2a1d5506e8fbcaeb56d9c84c4b687883f4559d9a
Umair khan <ukkhan5476@gmail.com>
2026-05-03 03:00:47 +0530

    feat: add Navbar component with styling for navigation links

--- Theme Switcher/src/components/Navbar.jsx
+++ Theme Switcher/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
       <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
       <Link to={"/blog"}>Blog</Link>
