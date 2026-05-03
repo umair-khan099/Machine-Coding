e3115b462022911cdc8f4d2ebedf106efdbabfa3
Umair khan <ukkhan5476@gmail.com>
2026-05-04 04:24:19 +0530

    feat: implement infinite scrolling functionality with data fetching

--- Infinit-Scrolling/src/App.jsx
+++ Infinit-Scrolling/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [item, setItem] = useState([]);
+  const [pages, setPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pages}`,
+    );
+
+    const data = await response.json();
+    setItem({ ...item, data });
+    console.log(item);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [pages]);
   return (
     <div>
+      <h1>Infinite Scrolling</h1>
+
       
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
