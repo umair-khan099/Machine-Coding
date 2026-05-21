0347423b7d872ae5848111cd5755b48da0492acd
Umair khan <ukkhan5476@gmail.com>
2026-05-22 03:03:34 +0530

    feat: initialize custom model project with React and Vite
    - Add package.json with dependencies and scripts for development
    - Create main application component (App) and model component (Model)
    - Set up entry point (main.jsx) for rendering the application
    - Include basic CSS file for styling
    - Configure Vite for React support

---
+++ custom Model/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Model from "./components/Model";
+
+const App = () => {
+  return (
+    <div>
+      
+        <Model />
+      </div>
+   
+  );
+};
+
+export default App;
---
+++ custom Model/src/components/Model.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Model = () => {
+  return (
+    <div className="model-main">
+      <div className="model-container">
+        <h1>umair</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. A dolorem
+          minima similique tempora sed odio, nihil autem aliquid, maiores ipsa
+          ratione beatae libero eaque eveniet at. Facere debitis explicabo qui?
+        </p>
+        <button>cencel</button>
+      </div>
+    </div>
+  );
+};
+
+export default Model;
